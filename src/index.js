8d7349ae10b4d0730c5acef53459472b1822ef93
liangjianfeng <769970587@qq.com>
2017-07-14 15:33:58 +0800

    [misc]Asset Management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 'use strict';
 
 import _ from 'lodash';
+import './style.css'
+import logosvg from './logo.svg'
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
+
+  var mysvg = new Image();
+  mysvg.src = logosvg;
+
+  element.appendChild(mysvg);
 
   return element;
 }
