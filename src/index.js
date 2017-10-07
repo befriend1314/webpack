613bb7517bbaf0c8acf2a74da728395b8c7f60ff
liangjianfeng <769970587@qq.com>
2017-10-07 14:26:44 +0800

    [update]

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-'use strict';
+'use strict'
 
-import _ from 'lodash';
-import './style.css'
-import logosvg from './logo.svg'
+import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
-  var element = document.createElement('div');
+  const element = document.createElement('div')
+  const btn = document.createElement('button')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  element.classList.add('hello');
-
-  var mysvg = new Image();
-  mysvg.src = logosvg;
-
-  element.appendChild(mysvg);
-
-  return element;
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
+  return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
